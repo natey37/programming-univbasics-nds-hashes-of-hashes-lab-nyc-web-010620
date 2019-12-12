8f7514b232603e492f6050c717b9091a60fd0861
Nathan Brickett <natey37@gmail.com>
2019-12-12 02:10:44 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -70,6 +70,6 @@ def naming_system
   
 PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
 PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
-PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
+PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
 PORTION_1
 end
